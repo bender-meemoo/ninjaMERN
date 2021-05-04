08e907d992271701aae2df22e666a842290a7fd9
Eric H. Smith <esmith167@gmail.com>
2021-05-03 22:53:04 -0400

    added senseui class. See READMD for full update.

--- ninjaMERN.js
+++ ninjaMERN.js
@@ -9,10 +9,26 @@ class Ninja {
         console.log(`My ninja name is ${this.name}!`);
     }
     showStats(){
-        console.log(`My power is great. Marvel at my abundant ${this.health} health, my quicksilver ${this.speed} dexterity, and my virile ${this.strength} brawn!`);
+        console.log(`My power is great, ${this.name} is my name. Marvel at my abundant ${this.health} health, my quicksilver ${this.speed} dexterity, and my virile ${this.strength} brawn!`);
     }
     drinkSake() {
         this.health += 10;
+    }
+}
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom() {
+       this.drinkSake
+        console.log(`${this.name} says, do not pee downwind from yourself.`);
+    }
+    showStats(){
+        console.log(`My power is great, ${this.name} is my name. Marvel at my abundant ${this.health} health, my quicksilver ${this.speed} dexterity, and my virile ${this.strength} brawn! I am also ${this.wisdom} wise like a wet fox!`);
     }
 }
 
@@ -21,3 +37,8 @@ ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
